54c7a3808cbbc957b950a6e8db46c972e1020d27
Dipak Roy <roydeepak1800@gmail.com>
2020-04-26 14:17:24 +0530

    done with this

---
+++ open-source-contribution/breakoutroomno-6/index.js
@@ -0,0 +1,7 @@
+let inp1=document.getElementById(inp-1);
+let inp2=document.getElementById(inp-2);
+let inp3=document.getElementById(inp-3);
+let targetbtn=document.getElementById('btn');
+targetbtn.addEventListener('click',()=>{
+    console.log(inp1, inp2, inp3);
+})
